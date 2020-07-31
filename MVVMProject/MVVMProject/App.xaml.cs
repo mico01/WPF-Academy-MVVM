2b645961ce49efa4774ff5361e9405c72904c45f
mico01 <54836074+mico01@users.noreply.github.com>
2020-07-31 11:41:15 +0200

    Add files via upload

---
+++ MVVMProject/MVVMProject/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using MVVMProject.Connectors;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MVVMProject
+{
+    /// <summary>
+    /// Lógica de interacción para App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private static DbConnector _dbConnector = new DbConnector();
+        public static DbConnector DbConnector
+        {
+            get { return App._dbConnector; }
+        }
+    }
+}
---
+++ MVVMProject/MVVMProject/Connectors/DbConnector.cs
@@ -0,0 +1,27 @@
+﻿using MVVMProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMProject.Connectors
+{
+    public class DbConnector
+    {
+        public PersonaCollection listarPersonas()
+        {
+            PersonaCollection lista = new PersonaCollection();
+            lista.Add(new Persona(1, "Pedro"));
+            lista.Add(new Persona(2, "Juan"));
+            lista.Add(new Persona(3, "Diego"));
+            lista.Add(new Persona(4, "Luis"));
+            return lista;
+        }
+        
+        public bool eliminarPersona(Persona p)
+        {
+            return true;
+        }
+    }
+}
